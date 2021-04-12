bf4c663b928c44b1945b3a8147b60e73297be45d
Rai Lopes <railopes89@gmail.com>
2021-04-12 19:18:29 -0300

    foi

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
+const observacoesPorLembreteId = {};
 app.use(express.json());
 //:id é um placeholder
 //exemplo: /lembretes/123456/observacoes
-app.put('/lembretes/:id/observacoes', (req, res) => {
+app.put('/lembretes/:id/observacoes', (req, res) => {});
+app.get('/lembretes/:id/observacoes', (req, res) => {});
+const observacoesPorLembreteId = {};
+const {
+    v4: uuidv4
+} = require('uuid');
+const idObs = uuidv4();
+const {
+    texto
+} = req.body;
+//req.params dá acesso à lista de parâmetros da URL
+const observacoesDoLembrete =
+    observacoesPorLembreteId[req.params.id] || [];
+observacoesDoLembrete.push({
+    id: idObs,
+    texto
 });
-app.get('/lembretes/:id/observacoes', (req, res) => {
-});
+observacoesPorLembreteId[req.params.id] =
+    observacoesDoLembrete;
+res.status(201).send(observacoesDoLembrete);
+
 app.listen(5000, (() => {
-console.log('Lembretes. Porta 5000');
+    console.log('Lembretes. Porta 5000');
 }));
